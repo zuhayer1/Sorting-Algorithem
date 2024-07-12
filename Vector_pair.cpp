4685ecae494a4583e359e97abfb3eeb32a5a0190
Zuhayer Anjum Barbhuiya <128217029+zuhayer1@users.noreply.github.com>
2024-07-12 22:26:04 +0530

    Update Vector_pair.cpp

--- Vector_pair.cpp
+++ Vector_pair.cpp
@@ -1,1 +1,13 @@
 
+// first element increasing - second element decreaing
+//global funciton
+bool sortbyCond(const pair<int, int>& a,
+                const pair<int, int>& b)
+{
+    if (a.x != b.x)
+        return (a.x < b.x);
+    else
+       return (a.y> b.y);
+}
+   
+sort(v.begin(), v.end(), sortbyCond);
