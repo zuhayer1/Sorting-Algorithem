064c9fbd7234d03946658d42a720d870965a5cb2
Zuhayer Anjum Barbhuiya <128217029+zuhayer1@users.noreply.github.com>
2025-05-21 16:46:29 +0530

    Update Vector_pair.cpp

--- Vector_pair.cpp
+++ Vector_pair.cpp
@@ -1,13 +1,35 @@
 
-// first element increasing - second element decreaing
+//First - INCR   < < < 
+// Second - INCR  < < < 
+sort(vp.begin(), vp.end(), [](const pair<ll, ll> &a, const pair<ll, ll> &b) {
+        if (a.first == b.first)
+            return a.second < b.second;
+        return a.first < b.first;
+});
+
+
+// first - INCR   < < < 
+// second - DECR  > > > 
 //global funciton
-bool sortbyCond(const pair<int, int>& a,
-                const pair<int, int>& b)
-{
-    if (a.x != b.x)
-        return (a.x < b.x);
-    else
-       return (a.y> b.y);
-}
-   
-sort(v.begin(), v.end(), sortbyCond);
+sort(v.begin(), v.end(), [](const pair<int, int> &a, const pair<int, int> &b) {
+        if (a.first == b.first)
+            return a.second > b.second; // Second decreasing
+        return a.first < b.first;       // First increasing
+ });
+
+// first - DECR > > > 
+// second - INCr < < < 
+sort(v.begin(), v.end(), [](const pair<int, int> &a, const pair<int, int> &b) {
+        if (a.first == b.first)
+            return a.second < b.second; // Second increasing
+        return a.first > b.first;       // First decreasing
+});
+
+
+// first - DEC > > > 
+// second - DEC > > >
+  sort(v.begin(), v.end(), [](const pair<int, int> &a, const pair<int, int> &b) {
+        if (a.first == b.first)
+            return a.second > b.second; // Second decreasing
+        return a.first > b.first;       // First decreasing
+    });
